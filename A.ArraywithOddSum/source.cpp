80988953fbdb455552560f94e538f535e2953705
Shashwath Nayak <159935998+ShashwathNayak06@users.noreply.github.com>
2026-03-06 21:48:20 +0530

    Create source.cpp

---
+++ A.ArraywithOddSum/source.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		int nodd = 0;
+		for(int i = 0;i < n;i++) {
+			int x;
+			cin >> x;
+			if(x % 2 != 0) nodd++;
+		}
+		if(nodd == 0 || (nodd == n && n % 2 == 0)) cout << "NO" << endl;
+		else cout << "YES" << endl;
+	}
+	return 0;
+}
